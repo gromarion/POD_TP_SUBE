1145945a8bf361a1ce240417110d4b900d1753b7
German Romarion <grromarion@hotmail.com>
2014-10-06 18:23:34 -0300

    further enhancement of code

--- server/src/main/java/ar/edu/itba/pod/mmxivii/sube/server/Cache.java
+++ server/src/main/java/ar/edu/itba/pod/mmxivii/sube/server/Cache.java
@@ -5,6 +5,7 @@ import java.rmi.server.UID;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.jgroups.Address;
 import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.ReceiverAdapter;
@@ -15,7 +16,7 @@ public class Cache extends ReceiverAdapter implements CardService {
 
 	private JChannel channel;
 	private String service_name;
-	private Map<UID, UserData> user_data;
+	private Map<UID, UserData> user_data; 
 
 	public Cache(String cluster_name, String service_name) throws Exception {
 		this.channel = new JChannel();
@@ -23,10 +24,6 @@ public class Cache extends ReceiverAdapter implements CardService {
 		this.channel.connect(cluster_name);
 		this.service_name = service_name;
 		this.user_data = new HashMap<UID, UserData>();
-	}
-
-	public void sendMessage(Message msg) throws Exception {
-		channel.send(msg);
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -42,14 +39,24 @@ public class Cache extends ReceiverAdapter implements CardService {
 
 	public void receive(Message msg) {
 		if (!msg.getSrc().equals(channel.getAddress())) {
+			if (msg.getObject() instanceof UID) {
+				UID id = (UID) msg.getObject();
+				UserData data = user_data.get(id);
+				if (data != null)
+					sendDataToEveryone(data);
+				else {
+					// Hablo con el server, y me contesta el balance de un flaco???
+					UserData new_data = new UserData(id, 0); // En lugar de 0 poner
+					// lo que devuelve
+					// el server
+					user_data.put(id, new_data);
+					sendDataToEveryone(new_data);
+				}
+			} else if(msg.getObject() instanceof UserData) {
+				UserData new_data = (UserData) msg.getObject();
+				user_data.put(new_data.getId(), new_data);
+			}
 		}
-	}
-
-	// PRIVATE METHODS
-
-	private boolean isLeader() {
-		return channel.getView().getMembers().get(0)
-				.equals(channel.getAddress());
 	}
 
 	@Override
@@ -58,8 +65,20 @@ public class Cache extends ReceiverAdapter implements CardService {
 		if (data != null) {
 			return data.getBalance();
 		} else {
-			// Leader must get info and send it back.
+			if (iAmTheLeader()) {
+				// Hablo con el server, y me contesta el balance de un flaco???
+				UserData new_data = new UserData(id, 0); // En lugar de 0 poner
+															// lo que devuelve
+															// el server
+				user_data.put(id, new_data);
+				sendDataToEveryone(new_data);
+			} else {
+				
+				askLeaderUserData(id);
+//				while(en operaciones a resolver no tenga la respueta);
+			}
 		}
+		return 0;
 	}
 
 	@Override
@@ -73,8 +92,9 @@ public class Cache extends ReceiverAdapter implements CardService {
 				// Notify balancer about error (throw exception???)
 			}
 		} else {
-			// Leader must get info and send it back.
+
 		}
+		return 0;
 	}
 
 	@Override
@@ -89,6 +109,38 @@ public class Cache extends ReceiverAdapter implements CardService {
 			}
 		} else {
 			// Leader must get info and send it back.
+		}
+		return 0;
+	}
+
+	// PRIVATE METHODS
+
+	private void askLeaderUserData(UID id) {
+		try {
+			channel.send(new Message(getLeader(), id));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private boolean isLeader() {
+		return channel.getView().getMembers().get(0)
+				.equals(channel.getAddress());
+	}
+
+	private boolean iAmTheLeader() {
+		return channel.getAddress().equals(getLeader());
+	}
+
+	private Address getLeader() {
+		return channel.getView().getMembers().get(0);
+	}
+	
+	private void sendDataToEveryone(Object o) {
+		try {
+			channel.send(new Message(null, null, o));
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
