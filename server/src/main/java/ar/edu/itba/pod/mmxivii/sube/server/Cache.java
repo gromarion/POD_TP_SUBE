29a81fb9749ff3268ba0f4aa3fa59aca56415ccb
German Romarion <grromarion@hotmail.com>
2014-10-12 15:32:45 -0300

    Removed coordinator. Everyone does everything now

--- server/src/main/java/ar/edu/itba/pod/mmxivii/sube/server/Cache.java
+++ server/src/main/java/ar/edu/itba/pod/mmxivii/sube/server/Cache.java
@@ -4,6 +4,7 @@ import java.rmi.RemoteException;
 import java.rmi.server.UID;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
@@ -11,6 +12,7 @@ import org.jgroups.Address;
 import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
 
 import ar.edu.itba.pod.mmxivii.sube.common.CardService;
 
@@ -18,14 +20,16 @@ public class Cache extends ReceiverAdapter implements CardService {
 
 	private JChannel channel;
 	private Map<UID, UserData> user_data;
-	private Map<UID, Queue<Operation>> pending_operations;
+	private Map<Address, Map<UID, Queue<Operation>>> pending_operations;
+	private Address controlling;
 
 	public Cache(String cluster_name, String service_name) throws Exception {
 		this.channel = new JChannel();
 		this.channel.setReceiver(this);
 		this.channel.connect(cluster_name);
 		this.user_data = new HashMap<UID, UserData>();
-		this.pending_operations = new HashMap<UID, Queue<Operation>>();
+		this.pending_operations = new HashMap<Address, Map<UID, Queue<Operation>>>();
+		this.controlling = addressToControl(true);
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -33,17 +37,22 @@ public class Cache extends ReceiverAdapter implements CardService {
 		System.out.println(cl.channel.getView().getMembers().get(0)
 				+ " is the leader.");
 		while (true) {
-			if (!cl.isLeader()) {
-				System.out.println(cl.getCardBalance(new UID((short) 100)));
-				break;
+			if (cl.controllingIsDead()) {
+				System.out.println(cl.controlling + " has died!");
+				cl.controlling = cl.addressToControl(false);
 			}
+			System.out.println("Now controlling " + cl.controlling);
 		}
 	}
 
 	public void receive(Message msg) {
 		if (!msg.getSrc().equals(channel.getAddress())) {
-			if (msg.getObject() instanceof UID) {
-				processInformationRequestAsLeader((UID) msg.getObject());
+			if (msg.getObject() instanceof Operation) {
+				Operation operation = (Operation) msg.getObject();
+				if (operation.isCompleted())
+					addPendingOperation(operation);
+				else
+					removePendingOperation(operation);
 			} else if (msg.getObject() instanceof UserData) {
 				UserData new_data = (UserData) msg.getObject();
 				user_data.put(new_data.userId(), new_data);
@@ -53,24 +62,16 @@ public class Cache extends ReceiverAdapter implements CardService {
 
 	@Override
 	public double getCardBalance(UID id) throws RemoteException {
-		System.out.println(id.toString());
+		Operation operation = new Operation(Type.GET, id,
+				this.channel.getAddress(), false);
+		informEveryoneAboutThisOperation(operation);
 		UserData data = user_data.get(id);
 		if (data != null) {
+			informEveryoneOfCompletedOperation(operation);
 			return data.balance();
 		} else {
-			if (iAmTheLeader()) {
-				processInformationRequestAsLeader(id);
-			} else {
-				addPendingOperation(id, new Operation(Type.GET));
-				askLeaderUserData(id);
-				while (user_data.get(id) == null
-						|| !user_data.get(id).isUpdated()) {
-					System.out.println("waiting for response");
-				}
-				return user_data.get(id).balance();
-			}
+			return 0;
 		}
-		return 0;
 	}
 
 	@Override
@@ -105,33 +106,75 @@ public class Cache extends ReceiverAdapter implements CardService {
 		return 0;
 	}
 
+	public void viewAccepted(View new_view) {
+		List<Address> members = new_view.getMembers();
+		if (controlling == null)
+			controlling = members.get(members.size() - 1);
+	}
+
+	public boolean controllingIsDead() {
+		return !this.channel.getView().getMembers().contains(controlling);
+	}
+
 	// PRIVATE METHODS
 
-	private void askLeaderUserData(UID id) {
-		try {
-			System.out.println("Asking leader for information...");
-			channel.send(new Message(getLeader(), id));
-		} catch (Exception e) {
-			e.printStackTrace();
+	private Address addressToControl(boolean first_connection) {
+		List<Address> members = this.channel.getView().getMembers();
+		if (first_connection) {
+			if (members.size() != 1)
+				return members.get(0);
+			else
+				return null;
+		} else {
+			int index_of_next_one_in_line = nextOneInLine(members);
+			if (members.get(index_of_next_one_in_line).equals(
+					this.channel.getAddress()))
+				return null;
+			return members.get(index_of_next_one_in_line);
 		}
 	}
 
-	private boolean isLeader() {
-		return channel.getView().getMembers().get(0)
-				.equals(channel.getAddress());
+	private int nextOneInLine(List<Address> members) {
+		boolean next_one_in_line = false;
+		int i = 0;
+		for (Address address : members) {
+			if (next_one_in_line) {
+				if (isLastInLine(members, address))
+					return 0;
+				else
+					return i;
+			} else if (address.equals(this.channel.getAddress())) {
+				next_one_in_line = true;
+			}
+			i++;
+		}
+		return 0;
 	}
 
-	private boolean iAmTheLeader() {
-		return channel.getAddress().equals(getLeader());
+	private boolean isLastInLine(List<Address> members, Address address) {
+		return members.get(members.size() - 1).equals(address);
 	}
 
-	private Address getLeader() {
-		return channel.getView().getMembers().get(0);
+	private void informEveryoneAboutThisOperation(Operation operation) {
+		sendDataToEveryone(operation);
 	}
 
-	private void addPendingOperation(UID id, Operation operation) {
-		if (pending_operations.containsKey(id)) {
-			pending_operations.get(id).add(operation);
+	private void informEveryoneOfCompletedOperation(Operation operation) {
+
+	}
+
+	private void addPendingOperation(Operation operation) {
+		if (pending_operations.containsKey(operation.address())) {
+			if (pending_operations.get(operation.address()).containsKey(
+					operation.userId()))
+				pending_operations.get(operation.address())
+						.get(operation.userId()).add(operation);
+			else {
+				LinkedList<Operation> queue = new LinkedList<Operation>();
+				queue.add(operation);
+				pending_operations.get(operation.address()).put(
+						operation.userId(), queue);
+			}
 		} else {
 			LinkedList<Operation> queue = new LinkedList<Operation>(); // TODO:
 																		// Verificar
@@ -140,23 +183,17 @@ public class Cache extends ReceiverAdapter implements CardService {
 																		// sea
 																		// ordenado.
 																		// CLAVE
-			queue.push(operation);
-			pending_operations.put(id, queue);
+			queue.add(operation);
+			Map<UID, Queue<Operation>> map = new HashMap<UID, Queue<Operation>>();
+			map.put(operation.userId(), queue);
+			pending_operations.put(operation.address(), map);
 		}
 	}
 
-	private void processInformationRequestAsLeader(UID id) {
-		System.out.print("Processing request...");
-		UserData data = user_data.get(id);
-		if (data != null) {
-			sendDataToEveryone(data);
-		} else {
-			UserData new_data = new UserData(id, TestRepo.getInstance()
-					.getBalance(id));
-			user_data.put(id, new_data);
-			sendDataToEveryone(new_data);
-		}
-		System.out.println("OK");
+	private void removePendingOperation(Operation operation) {
+		Queue<Operation> address_pending_operations = pending_operations.get(operation.address()).get(operation.userId());
+		if (address_pending_operations.peek().equals(operation))
+			address_pending_operations.poll();
 	}
 
 	private void sendDataToEveryone(Object o) {
--- server/src/main/java/ar/edu/itba/pod/mmxivii/sube/server/Operation.java
+++ server/src/main/java/ar/edu/itba/pod/mmxivii/sube/server/Operation.java
@@ -1,13 +1,42 @@
 package ar.edu.itba.pod.mmxivii.sube.server;
 
-import java.sql.Time;
+import java.rmi.server.UID;
+import java.util.Date;
+
+import org.jgroups.Address;
 
 public class Operation {
-	private Time time;
+	private Date date;
+	private UID user_id;
+	private Address address;
 	private Type type;
-	
-	public Operation(Type type) {
+	private boolean completed;
+
+	public Operation(Type type, UID user_id, Address address, boolean completed) {
 		this.type = type;
-//		this.time = new Time;
+		this.user_id = user_id;
+		this.address = address;
+		this.date = new Date();
+		this.completed = completed;
+	}
+
+	public UID userId() {
+		return this.user_id;
+	}
+
+	public Type type() {
+		return this.type;
+	}
+
+	public Date date() {
+		return this.date;
+	}
+
+	public boolean isCompleted() {
+		return completed;
+	}
+	
+	public Address address() {
+		return address;
 	}
 }
