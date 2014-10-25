c60a06815e6a172c34b073cee00f6db1bf06987f
Gonzalo Castiglione <gcastigl@gmail.com>
2014-10-25 09:41:54 -0200

    con el thread extra de los synchronizers, sfaltaba considerar los errores de concurrencia

--- service/src/main/java/ar/edu/itba/pod/mmxivii/sube/entity/CachedData.java
+++ service/src/main/java/ar/edu/itba/pod/mmxivii/sube/entity/CachedData.java
@@ -24,27 +24,37 @@ public class CachedData implements Serializable {
 	}
 
 	public UserData get(UID uid) {
-		return checkNotNull(_userdatas.get(uid));
+		synchronized (_userdatas) {
+			return checkNotNull(_userdatas.get(uid));
+		}
 	}
 
 	public Optional<UserData> tryGet(UID uid) {
-		return Optional.fromNullable(_userdatas.get(uid));
+		synchronized (_userdatas) {
+			return Optional.fromNullable(_userdatas.get(uid));
+		}
 	}
 
 	public CachedData put(UID uid, UserData userdata) {
-		UserData previous = _userdatas.put(uid, userdata);
-		checkArgument(previous == null);
-		return this;
+		synchronized (_userdatas) {
+			UserData previous = _userdatas.put(uid, userdata);
+			checkArgument(previous == null);
+			return this;
+		}
 	}
 
 	public void setTo(CachedData other) {
-		checkState(_userdatas.isEmpty());
-		_userdatas.putAll(other._userdatas);
+		synchronized (_userdatas) {
+			checkState(_userdatas.isEmpty());
+			_userdatas.putAll(other._userdatas);
+		}
 	}
 
 	public void clearAll(CachedData data) {
-		for (Entry<UID, UserData> entry : data._userdatas.entrySet()) {
-			_userdatas.get(entry.getKey()).operations().removeAll(entry.getValue().operations());
+		synchronized (_userdatas) {
+			for (Entry<UID, UserData> entry : data._userdatas.entrySet()) {
+				_userdatas.get(entry.getKey()).operations().removeAll(entry.getValue().operations());
+			}
 		}
 	}
 }
